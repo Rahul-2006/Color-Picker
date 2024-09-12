dfba94795f51543b9df4a978844f240a91bf896e
Rahul <87422105+Rahul-2006@users.noreply.github.com>
2024-09-12 14:25:27 +0530

    Revert "FixedVisibilitySelectedColor"

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 function ColorPicker() {
     
     const [color , setColor] = useState("#FFFFFF");
@@ -6,22 +6,10 @@ function ColorPicker() {
         setColor(event.target.value);
     }
 
-    const getTextColor = (hex) => {
-        const r = parseInt(hex.slice(1, 3), 16);
-        const g = parseInt(hex.slice(3, 5), 16);
-        const b = parseInt(hex.slice(5, 7), 16);
-
-        const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-
-        return brightness < 150 ? '#FFFFFF' : '#000000';
-    };
-
-  const textColor = getTextColor(color);
-
     return( <div className="color-picker-container">
             <h1> Color Picker</h1>
             <div className="color-display" style={{backgroundColor : color}}>
-                <p  style={{backgroundColor: "transparent", color: textColor}}>Selected Color: {color}</p>
+                <p>Selected Color: {color}</p>
             </div>
             <label>Select a Color:</label>
             <input type="color" value={color} onChange={handleColorChange}/>
